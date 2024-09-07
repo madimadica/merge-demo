02ab96042ad00d959edd90d6a5c6c6ab9815f2db
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-09-07 15:25:45 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+arr1 = [
+    "alpha",
+    "bravo",
+    "charlie",
+    "delta",
+    "echo",
+    "foxtrot",
+    "golf",
+    "hotel"
+]
+
+
+arr2 = [
+    "Alpha",
+    "Bravo",
+    "Charlie",
+    "Delta",
+    "Echo",
+    "Foxtrot",
+    "Golf",
+    "Hotel"
+]
